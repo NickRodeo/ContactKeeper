fa2f8e9bf9b6b1e69c9202800da21774f4ded374
NickRodeo <rodeonick708@gmail.com>
2025-06-14 13:51:44 +0700

    Fix Mobile lagi

--- dist/src/backend/controller/UIController.js
+++ dist/src/backend/controller/UIController.js
@@ -21,7 +21,7 @@ class UIController {
   <html>
     <head>
       <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
       <title>${title}</title>
       <link rel="stylesheet" href="/tailwind/output.css" />
     </head>
